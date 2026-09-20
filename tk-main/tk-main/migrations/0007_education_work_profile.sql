8f4ce041875a0e10d74df850a48c47dfa0d77ae2
buitanloc-2310 <buitanloccon@gmail.com>
2026-09-20 22:42:12 +0700

    Delete tk-main directory

--- tk-main/tk-main/migrations/0007_education_work_profile.sql
+++
@@ -1,6 +0,0 @@
--- Add education/workplace profile fields without changing existing data.
-ALTER TABLE people ADD COLUMN education_or_work_type TEXT;
-ALTER TABLE people ADD COLUMN school_or_workplace TEXT;
-ALTER TABLE people ADD COLUMN class_or_major TEXT;
-ALTER TABLE people ADD COLUMN education_status TEXT;
-INSERT OR IGNORE INTO schema_version(version) VALUES(7);
